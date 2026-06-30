9c600ea02f2a38a2344c1b099eaf74206e797f3d
guanhua <0guanhua0@gmail.com>
2026-06-29 22:26:12 -0700

    6

--- metal.py
+++ metal.py
@@ -43,10 +43,10 @@ using namespace metal;
 kernel void k2(device float *d0, device float *d1, uint3 gid [[threadgroup_position_in_grid]], uint3 lid [[thread_position_in_threadgroup]]) {{
     size_t lid0 = lid.x;
     threadgroup float reduce[{local_size[0]}];
-    size_t BLOCK = ({n} + {local_size[0]} - 1) / {local_size[0]};
+    size_t BLOCK = ({n_pad} + {local_size[0]} - 1) / {local_size[0]};
 
     float max_val = -FLT_MAX;
-    for (size_t i = lid0 * BLOCK; i < min((lid0 + 1) * BLOCK, (size_t){n}); ++i) {{
+    for (size_t i = lid0 * BLOCK; i < min((lid0 + 1) * BLOCK, (size_t){n_pad}); ++i) {{
         max_val = fmax(max_val, d1[i]);
     }}
     reduce[lid0] = max_val;
@@ -60,7 +60,7 @@ kernel void k2(device float *d0, device float *d1, uint3 gid [[threadgroup_posit
     threadgroup_barrier(mem_flags::mem_threadgroup);
     max_val = reduce[0];
     float sum_exp = 0.0f;
-    for (size_t i = lid0 * BLOCK; i < min((lid0 + 1) * BLOCK, (size_t){n}); ++i) {{
+    for (size_t i = lid0 * BLOCK; i < min((lid0 + 1) * BLOCK, (size_t){n_pad}); ++i) {{
         sum_exp += exp(d1[i] - max_val);
     }}
     reduce[lid0] = sum_exp;
@@ -73,7 +73,7 @@ kernel void k2(device float *d0, device float *d1, uint3 gid [[threadgroup_posit
 
     threadgroup_barrier(mem_flags::mem_threadgroup);
     sum_exp = reduce[0];
-    for (size_t i = lid0 * BLOCK; i < min((lid0 + 1) * BLOCK, (size_t){n}); ++i) {{
+    for (size_t i = lid0 * BLOCK; i < min((lid0 + 1) * BLOCK, (size_t){n_pad}); ++i) {{
         d0[i] = exp(d1[i] - max_val) / sum_exp;
     }}
 }}
@@ -89,7 +89,7 @@ kernel void k3(device float *d0, device float *d1, uint3 gid [[threadgroup_posit
     threadgroup float reduce[{local_size[0]}];
 
     float max_val = -FLT_MAX;
-    for (size_t i = lid0; i < (size_t){n}; i += {local_size[0]}) {{
+    for (size_t i = lid0; i < (size_t){n_pad}; i += {local_size[0]}) {{
         max_val = fmax(max_val, d1[i]);
     }}
     reduce[lid0] = max_val;
@@ -103,7 +103,7 @@ kernel void k3(device float *d0, device float *d1, uint3 gid [[threadgroup_posit
     threadgroup_barrier(mem_flags::mem_threadgroup);
     max_val = reduce[0];
     float sum_exp = 0.0f;
-    for (size_t i = lid0; i < (size_t){n}; i += {local_size[0]}) {{
+    for (size_t i = lid0; i < (size_t){n_pad}; i += {local_size[0]}) {{
         sum_exp += exp(d1[i] - max_val);
     }}
     reduce[lid0] = sum_exp;
@@ -116,7 +116,7 @@ kernel void k3(device float *d0, device float *d1, uint3 gid [[threadgroup_posit
 
     threadgroup_barrier(mem_flags::mem_threadgroup);
     sum_exp = reduce[0];
-    for (size_t i = lid0; i < (size_t){n}; i += {local_size[0]}) {{
+    for (size_t i = lid0; i < (size_t){n_pad}; i += {local_size[0]}) {{
         d0[i] = exp(d1[i] - max_val) / sum_exp;
     }}
 }}
@@ -133,7 +133,7 @@ kernel void k4(device float *d0, device float *d1, uint3 gid [[threadgroup_posit
     threadgroup float reduce[{local_size[0]} / 32];
 
     float max_val = -FLT_MAX;
-    for (size_t i = lid0; i < (size_t){n}; i += {local_size[0]}) {{
+    for (size_t i = lid0; i < (size_t){n_pad}; i += {local_size[0]}) {{
         max_val = fmax(max_val, d1[i]);
     }}
     max_val = simd_max(max_val);
@@ -153,7 +153,7 @@ kernel void k4(device float *d0, device float *d1, uint3 gid [[threadgroup_posit
     threadgroup_barrier(mem_flags::mem_threadgroup);
     max_val = reduce[0];
     float sum_exp = 0.0f;
-    for (size_t i = lid0; i < (size_t){n}; i += {local_size[0]}) {{
+    for (size_t i = lid0; i < (size_t){n_pad}; i += {local_size[0]}) {{
         sum_exp += exp(d1[i] - max_val);
     }}
     sum_exp = simd_sum(sum_exp);
@@ -172,7 +172,7 @@ kernel void k4(device float *d0, device float *d1, uint3 gid [[threadgroup_posit
 
     threadgroup_barrier(mem_flags::mem_threadgroup);
     sum_exp = reduce[0];
-    for (size_t i = lid0; i < (size_t){n}; i += {local_size[0]}) {{
+    for (size_t i = lid0; i < (size_t){n_pad}; i += {local_size[0]}) {{
         d0[i] = exp(d1[i] - max_val) / sum_exp;
     }}
 }}
@@ -192,7 +192,7 @@ kernel void k5(device float *d0, device float *d1, uint3 gid [[threadgroup_posit
     float max_val = -FLT_MAX;
     float max_val_old = max_val;
     float sum_exp = 0.0f;
-    for (size_t i = lid0; i < (size_t){n}; i += {local_size[0]}) {{
+    for (size_t i = lid0; i < (size_t){n_pad}; i += {local_size[0]}) {{
         max_val = fmax(max_val, d1[i]);
         if (max_val > max_val_old) {{
             sum_exp *= exp(max_val_old - max_val);
@@ -226,8 +226,70 @@ kernel void k5(device float *d0, device float *d1, uint3 gid [[threadgroup_posit
     threadgroup_barrier(mem_flags::mem_threadgroup);
     max_val = reduce_max[0];
     sum_exp = reduce_sum[0];
-    for (size_t i = lid0; i < (size_t){n}; i += {local_size[0]}) {{
+    for (size_t i = lid0; i < (size_t){n_pad}; i += {local_size[0]}) {{
         d0[i] = exp(d1[i] - max_val) / sum_exp;
+    }}
+}}
+"""
+
+
+k6 = """
+#include <metal_stdlib>
+using namespace metal;
+
+kernel void k6(device float *d0, device float *d1, uint3 gid [[threadgroup_position_in_grid]], uint3 lid [[thread_position_in_threadgroup]]) {{
+    size_t lid0 = lid.x;
+    size_t simd_id = lid0 / 32;
+    threadgroup float reduce_max[{local_size[0]} / 32];
+    threadgroup float reduce_sum[{local_size[0]} / 32];
+
+    float max_val = -FLT_MAX;
+    float max_val_old = max_val;
+    float sum_exp = 0.0f;
+    for (size_t i = lid0; i < (size_t)({n_pad} / 4); i += {local_size[0]}) {{
+        float4 val = ((device float4 *)d1)[i];
+        max_val = fmax(max_val, val.x);
+        max_val = fmax(max_val, val.y);
+        max_val = fmax(max_val, val.z);
+        max_val = fmax(max_val, val.w);
+        if (max_val > max_val_old) {{
+            sum_exp *= exp(max_val_old - max_val);
+            max_val_old = max_val;
+        }}
+        sum_exp += exp(val.x - max_val);
+        sum_exp += exp(val.y - max_val);
+        sum_exp += exp(val.z - max_val);
+        sum_exp += exp(val.w - max_val);
+    }}
+    max_val = simd_max(max_val);
+    sum_exp *= exp(max_val_old - max_val);
+    sum_exp = simd_sum(sum_exp);
+    if (lid0 % 32 == 0) {{
+        reduce_max[simd_id] = max_val;
+        reduce_sum[simd_id] = sum_exp;
+    }}
+
+    threadgroup_barrier(mem_flags::mem_threadgroup);
+    if (simd_id == 0) {{
+        max_val = lid0 < {local_size[0]} / 32 ? reduce_max[lid0] : -FLT_MAX;
+        max_val_old = max_val;
+        sum_exp = lid0 < {local_size[0]} / 32 ? reduce_sum[lid0] : 0.0f;
+
+        max_val = simd_max(max_val);
+        sum_exp *= exp(max_val_old - max_val);
+        sum_exp = simd_sum(sum_exp);
+        if (lid0 == 0) {{
+            reduce_max[0] = max_val;
+            reduce_sum[0] = sum_exp;
+        }}
+    }}
+
+    threadgroup_barrier(mem_flags::mem_threadgroup);
+    max_val = reduce_max[0];
+    sum_exp = reduce_sum[0];
+    for (size_t i = lid0; i < (size_t)({n_pad} / 4); i += {local_size[0]}) {{
+        float4 val = ((device float4 *)d1)[i];
+        ((device float4 *)d0)[i] = exp(val - max_val) / sum_exp;
     }}
 }}
 """
@@ -241,24 +303,27 @@ def _sched(
     global_size: list[int],
     local_size: list[int],
 ) -> np.ndarray:
+    n_pad = (n + 3) & ~3
+    d0_buf, d1_buf = metalalloc.alloc(n_pad * 4), metalalloc.alloc(n_pad * 4)
+    data_pad = np.pad(data, (0, n_pad - n), constant_values=np.finfo(np.float32).min)
+    metalalloc._copyin(d1_buf, data_pad.tobytes())
+
     prog = MetalProgram(
         Device[Device.DEFAULT],
         name,
-        compile(kernel.format(n=n, global_size=global_size, local_size=local_size)),
+        compile(
+            kernel.format(n_pad=n_pad, global_size=global_size, local_size=local_size)
+        ),
     )
-
-    d0_buf, d1_buf = metalalloc.alloc(n * 4), metalalloc.alloc(n * 4)
-    metalalloc._copyin(d1_buf, data.tobytes())
-
     prog(d0_buf, d1_buf, global_size=global_size, local_size=local_size, wait=True)
 
-    d0 = np.empty((n), dtype=np.float32)
+    d0 = np.empty((n_pad), dtype=np.float32)
     metalalloc._copyout(flat_mv(d0.data), d0_buf)
 
-    metalalloc.free(d0_buf, n * 4)
-    metalalloc.free(d1_buf, n * 4)
+    metalalloc.free(d0_buf, n_pad * 4)
+    metalalloc.free(d1_buf, n_pad * 4)
 
-    return d0
+    return d0[:n]
 
 
 if __name__ == "__main__":
@@ -267,5 +332,6 @@ if __name__ == "__main__":
         "3": partial(_sched, "k3", k3),
         "4": partial(_sched, "k4", k4),
         "5": partial(_sched, "k5", k5),
+        "6": partial(_sched, "k6", k6),
     }
     perf.run(kernel)
